a383cffc94ced4b9cb996abb2d5790397c9547c1
Luca Di Fazio <highliuk@gmail.com>
2024-06-07 16:54:04 +0200

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,236 @@
+# WordPress Command
+
+An easy way to define custom commands in WordPress powered by Symfony Console instead of WP-CLI.
+
+## Installation
+
+Use composer to install the package:
+
+```bash
+composer require highliuk/wordpress-command
+```
+
+## Usage
+
+First, create an entrypoint script in your WordPress installation directory. You may call it `artisan`, `console` or whatever you like. Here is an example of such a script:
+
+```php
+if (php_sapi_name() !== 'cli') {
+    exit(1);
+}
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/cms/wp-load.php';
+
+use Symfony\Component\Console\Application;
+
+$app = new Application();
+$app->run();
+```
+
+Then, create your custom command by extending the `WordPressCommand` class:
+
+```php
+use Highliuk\WordPressCommand\WordPressCommand;
+
+/**
+ * Greets the blog with its name.
+ */
+class HelloBlog extends WordPressCommand
+{
+    protected function handle(): void
+    {
+        $name = get_bloginfo('name');
+
+        $this->output->writeln("Hello, $name!");
+    }
+}
+```
+
+Finally, register your command in the entrypoint script:
+
+```php
+$app->add(new HelloBlog());
+```
+
+Now you can run your custom command:
+
+```bash
+php artisan hello:blog
+# Hello, My Blog!
+```
+
+You have access to all of the Symfony Console features, such as options and arguments. See the [Symfony Console documentation](https://symfony.com/doc/current/components/console.html) for more information.
+
+### Customization
+
+You can customize the command by overriding the `setup` method:
+
+```php
+use Highliuk\WordPressCommand\WordPressCommand;
+
+class HelloBlog extends WordPressCommand
+{
+    protected function setup(): void
+    {
+        $this->setName('greet:blog');
+    }
+
+    protected function handle(): void
+    {
+        $name = get_bloginfo('name');
+
+        $this->output->writeln("Hello, $name!");
+    }
+}
+```
+
+By default, the command name is inferred from the class name. For instance, the `HelloBlog` command will be available as `hello:blog`. Similarly, the command description is inferred from the class docblock. If you want to customize the command name and description, you can use the `setName` and `setDescription` methods in the `setup` method, or you can use the shorthand properties:
+
+```php
+use Highliuk\WordPressCommand\WordPressCommand;
+
+class HelloBlog extends WordPressCommand
+{
+    protected $name = 'greet:blog';
+    protected $description = 'Greets the blog with its name.';
+
+    protected function handle(): void
+    {
+        $name = get_bloginfo('name');
+
+        $this->output->writeln("Hello, $name!");
+    }
+}
+```
+
+### Registering Commands
+
+If you want to decentralize where your commands are registered, the best way to do this is to hook into a custom action:
+
+```php
+// artisan
+$app = new Application();
+
+do_action('your_custom_hook', $app);
+
+$app->run();
+
+// functions.php
+add_action('your_custom_hook', function (Application $app) {
+    $app->add(new HelloBlog());
+});
+```
+
+## Features
+
+### Argument and Option Bindings
+
+You can access arguments and options from your handle method:
+
+```php
+use Highliuk\WordPressCommand\WordPressCommand;
+use Symfony\Component\Console\Input\InputArgument;
+
+class GreetUser extends WordPressCommand
+{
+    protected function setup(): void
+    {
+        $this
+            ->addArgument('user', InputArgument::REQUIRED, 'The user to greet')
+            ->addOption('uppercase', 'u', 'Whether to uppercase the user name');
+    }
+
+    protected function handle(string $user, bool $uppercase): void
+    {
+        if ($uppercase) {
+            $user = strtoupper($user);
+        }
+
+        $this->output->writeln("Hello, $user!");
+    }
+}
+```
+
+```bash
+php artisan greet:user john -u
+# Hello, JOHN!
+```
+
+### Multisite Support
+
+By default, the command will run on the main site. If you want to run the command on specific sites, you can use the `--blogs` option:
+
+```bash
+php artisan hello:blog --blogs=1,2,3
+```
+
+Or you can run the command on all sites by using the `--all-sites` option:
+
+```bash
+php artisan hello:blog --all-sites
+```
+
+To use the `--all-sites` option, you explicitly need to enable it via the `allowToRunCommandOnAllSites` method:
+
+```php
+use Highliuk\WordPressCommand\WordPressCommand;
+
+class HelloBlog extends WordPressCommand
+{
+    protected function setup(): void
+    {
+        $this->allowToRunCommandOnAllSites();
+    }
+
+    protected function handle(): void
+    {
+        $name = get_bloginfo('name');
+
+        $this->output->writeln("Hello, $name!");
+    }
+}
+```
+
+If you want to exclude specific sites, you can use the `--skip-blogs` option:
+
+```bash
+php artisan hello:blog --all-sites --skip-blogs=1,2,3
+```
+
+If your command should **never** run on the main site, you can use the `skipCommandOnMainSite` method, so that the command will only run on subsites:
+
+```php
+use Highliuk\WordPressCommand\WordPressCommand;
+
+class HelloBlog extends WordPressCommand
+{
+    protected function setup(): void
+    {
+        $this->skipCommandOnMainSite();
+    }
+
+    protected function handle(): void
+    {
+        $name = get_bloginfo('name');
+
+        $this->output->writeln("Hello, $name!");
+    }
+}
+```
+
+## Troubleshooting
+
+### PHP Warning: Undefined array key "HTTP_HOST"
+
+In your `wp-config.php` file, add the following line just after the `DOMAIN_CURRENT_SITE` definition:
+
+```php
+if (empty($_SERVER['HTTP_HOST'])) {
+    $_SERVER['HTTP_HOST'] = DOMAIN_CURRENT_SITE;
+}
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/WordPressCommand.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace HighLiuk\WordPressCommand;
+
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionNamedType;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+/**
+ * Base class for WordPress commands.
+ */
+class WordPressCommand extends Command
+{
+    /**
+     * The input instance.
+     */
+    protected InputInterface $input;
+
+    /**
+     * The output instance.
+     */
+    protected OutputInterface $output;
+
+    /**
+     * Blog IDs to skip.
+     *
+     * @var int[]
+     */
+    private array $skip_blogs = [];
+
+    /**
+     * The command name.
+     *
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * The command description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    protected function configure(): void
+    {
+        if ($name = $this->name ?: $this->inferCommandName()) {
+            $this->setName($name);
+        }
+
+        if ($description = $this->description ?: $this->inferCommandDescription()) {
+            $this->setDescription($description);
+        }
+
+        $this
+            ->addOption('blogs', 'b', InputOption::VALUE_OPTIONAL, 'The IDs of the blogs (comma-separated)', '1')
+            ->setup();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->input = $input;
+        $this->output = $output;
+
+        foreach ($this->getBlogIds() as $blog_id) {
+            switch_to_blog($blog_id);
+
+            try {
+                $this->{'handle'}(...$this->getHandleArgs());
+            } catch (Throwable $e) {
+                $this->output->writeln("<error>{$e->getMessage()}</error>");
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Resolve the arguments and options for the handle method.
+     *
+     * @return mixed[]
+     */
+    private function getHandleArgs(): array
+    {
+        $reflection = new ReflectionMethod($this, 'handle');
+        $parameters = $reflection->getParameters();
+        $args = [];
+
+        foreach ($parameters as $parameter) {
+            $name = $parameter->getName();
+            $name = str_replace('_', '-', $name);
+
+            // resolve the argument value
+            if ($this->input->hasArgument($name)) {
+                $arg = $this->input->getArgument($name);
+            } elseif ($this->input->hasOption($name)) {
+                $arg = $this->input->getOption($name);
+            } elseif ($parameter->isDefaultValueAvailable()) {
+                $arg = $parameter->getDefaultValue();
+            } else {
+                $arg = null;
+            }
+
+            // resolve the argument type
+            if ($parameter->hasType()) {
+                $type = $parameter->getType();
+
+                if ($type instanceof ReflectionNamedType && $type->isBuiltin()) {
+                    settype($arg, $type->getName());
+                }
+            }
+
+            $args[] = $arg;
+        }
+
+        return $args;
+    }
+
+    /**
+     * Infer the command name from the class name.
+     */
+    private function inferCommandName(): ?string
+    {
+        $reflection = new ReflectionClass($this);
+        $className = $reflection->getShortName();
+
+        $className = preg_replace('/([a-z])([A-Z])/', '$1-$2', $className);
+        if ($className === null) {
+            return null;
+        }
+        $className = strtolower($className);
+        $className = preg_replace('/-/', ':', $className, 1);
+
+        return $className;
+    }
+
+    /**
+     * Infer the command description from the doc comment of the class.
+     */
+    private function inferCommandDescription(): ?string
+    {
+        $reflection = new ReflectionClass($this);
+
+        if (! $docComment = $reflection->getDocComment()) {
+            return null;
+        }
+
+        $description = preg_replace('/(^[\/\*\s]*|[\s\*\/]*$)/', '', $docComment);
+
+        if ($description === null) {
+            return null;
+        }
+
+        return trim($description);
+
+    }
+
+    /**
+     * Get the blog IDs from the input.
+     *
+     * @return int[]
+     */
+    private function getBlogIds(): array
+    {
+        // if the --skip-blogs option is provided, skip the blogs with the provided IDs
+        if ($this->input->hasOption('skip-blogs') && $this->input->getOption('skip-blogs')) {
+            $skip_blogs = explode(',', (string) $this->input->getOption('skip-blogs'));
+            $skip_blogs = array_map('intval', $skip_blogs);
+
+            $this->skip_blogs = [...$this->skip_blogs, ...$skip_blogs];
+        }
+
+        // if the --all-sites option is provided, get all the blog IDs. Otherwise, get
+        // the blog IDs from the --blogs option
+        if ($this->input->hasOption('all-sites') && $this->input->getOption('all-sites')) {
+            $blog_ids = get_sites([
+                'fields' => 'ids',
+                'archived' => 0,
+                'deleted' => 0,
+            ]);
+        } else {
+            $blog_ids = explode(',', (string) $this->input->getOption('blogs'));
+        }
+
+        // exclude any blog IDs from the skip list in the blog IDs list
+        $blog_ids = array_map('intval', $blog_ids);
+        $blog_ids = array_diff($blog_ids, $this->skip_blogs);
+        $blog_ids = array_filter($blog_ids);
+        $blog_ids = array_values($blog_ids);
+
+        return $blog_ids;
+    }
+
+    /**
+     * Allow to run the command on all sites by using the --all-sites option.
+     *
+     * @return $this
+     */
+    protected function allowToRunCommandOnAllSites(): static
+    {
+        return $this
+            ->addOption('all-sites', null, InputOption::VALUE_NONE, 'Run the command on all sites')
+            ->addOption('skip-blogs', null, InputOption::VALUE_OPTIONAL, 'The IDs of the blog(s) to skip (comma-separated)');
+    }
+
+    /**
+     * Skips the command on the main site.
+     */
+    protected function skipCommandOnMainSite(): static
+    {
+        $this->skip_blogs[] = 1;
+
+        return $this;
+    }
+
+    /**
+     * Setup the command.
+     */
+    protected function setup(): void
+    {
+        //
+    }
+}
